73d0c399b95d2056a8e49859e6cfd9c943504a03
lufia <lufia@lufia.org>
2020-01-13 16:40:54 +0900

    cmd/ping: use mackerel exporter

--- cmd/ping/main.go
+++ cmd/ping/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/key"
 	"go.opentelemetry.io/otel/api/metric"
+
+	"github.com/lufia/mackerelexporter"
 )
 
 // https://github.com/open-telemetry/opentelemetry-go/blob/master/sdk/metric/example_test.go
 
 func main() {
+	log.SetFlags(0)
+	pusher, err := mackerel.InstallNewPipeline()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pusher.Close()
+
 	meter := global.MeterProvider().Meter("example/ping")
-	key := key.New("tick")
+	key := key.New("host.id")
 	counter := meter.NewInt64Counter("a.counter", metric.WithKeys(key))
 	labels := meter.Labels(key.String("value"))
 	ctx := context.Background()
@@ -23,4 +34,5 @@ func main() {
 
 	m := counter.Measurement(1)
 	meter.RecordBatch(ctx, labels, m)
+	time.Sleep(2 * time.Minute)
 }
